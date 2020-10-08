39c596f85caffa625c6e46ba6abf1c457af7173f
koduki <pascalm3+git@gmail.com>
2020-10-08 02:05:54 -0700

    Modify UI

--- tasknotes-web/src/views/Ticket.vue
+++ tasknotes-web/src/views/Ticket.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container" style="text-align: left">
+  <div class="container-fluid" style="text-align: left">
     <div class="align-items-center pt-3 pb-2 mb-3 border-bottom">
       <div>
         <b-button style="margin: 5px">
@@ -13,23 +13,45 @@
       </div>
     </div>
 
-    <h2 class="h4">Details:</h2>
-    <table class="table">
-      <tbody>
-        <tr>
-          <th scope="col">ステータス</th>
-          <td>{{ task.status != "" ? task.status : "Open" }}</td>
-          <th scope="col">期日</th>
-          <td>{{ task.dueDate != "" ? task.dueDate : "-" }}</td>
-        </tr>
-      </tbody>
-    </table>
-    <h2 class="h4">Description & Activity:</h2>
-    <main role="main" class="col-md-12">
-      <div>
-        <vue-simplemde v-model="task.body" ref="markdownEditor" />
-      </div>
-    </main>
+    <div class="row">
+      <nav
+        class="col-md-3 d-none d-md-block bg-light sidebar"
+        style="margin: 20px"
+      >
+        <div class="sidebar-sticky">
+          <ul class="nav flex-column">
+            <li
+              class="nav-item"
+              v-for="task in this.$store.state.tasks"
+              :key="task._id"
+            >
+              <span @onclick="onClickTicketItem(task)"
+                >{{ task.id }} {{ task.name }}</span
+              >
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <table class="table col-md-6" style="margin: 20px">
+        <h2 class="h4">Details:</h2>
+        <table class="table">
+          <tbody>
+            <tr>
+              <th scope="col">ステータス</th>
+              <td>{{ task.status != "" ? task.status : "Open" }}</td>
+              <th scope="col">期日</th>
+              <td>{{ task.dueDate != "" ? task.dueDate : "-" }}</td>
+            </tr>
+          </tbody>
+        </table>
+        <h2 class="h4">Description & Activity:</h2>
+        <main role="main" class="col-md-12">
+          <div>
+            <vue-simplemde v-model="task.body" ref="markdownEditor" />
+          </div>
+        </main>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -49,8 +71,6 @@ export default {
   },
 
   created() {
-    this.$store.state.tasks;
-    this.$route.params.id;
     this.$store.state.tasks.forEach((t) => {
       if (t.id == this.$route.params.id) {
         this.task = t;
@@ -61,6 +81,14 @@ export default {
   mounted() {
     this.$refs.markdownEditor.simplemde.togglePreview();
   },
-  methods: {},
+  methods: {
+    onClickTicketItem(t) {
+      this.$store.state.tasks.forEach((t) => {
+        if (t.id == this.$route.params.id) {
+          this.task = t;
+        }
+      });
+    },
+  },
 };
 </script>
--- tasknotes-web/src/views/Tickets.vue
+++ tasknotes-web/src/views/Tickets.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container" style="text-align: left">
+  <div class="container-fluid" style="text-align: left">
     <div class="align-items-center pt-3 pb-2 mb-3 border-bottom">
       <div>
         <b-button style="margin: 5px">
@@ -13,29 +13,33 @@
       </div>
     </div>
 
-    <table class="table" style="text-align=">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Name</th>
-          <th scope="col">ステータス</th>
-          <th scope="col">期日</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="task in this.$store.state.tasks" :key="task._id">
-          <th>
-            <router-link :to="'/tickets/' + task.id">{{ task.id }}</router-link>
-          </th>
-          <td>
-            <router-link :to="'/tickets/' + task.id">
-              {{ task.name }}
-            </router-link>
-          </td>
-          <td>{{ task.status != "" ? task.status : "Open" }}</td>
-          <td>{{ task.dueDate != "" ? task.dueDate : "-" }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="row">
+      <table class="table col-md-6" style="margin: 20px">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col" width="150px">ステータス</th>
+            <th scope="col" width="150px">期日</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="task in this.$store.state.tasks" :key="task._id">
+            <th>
+              <router-link :to="'/tickets/' + task.id">{{
+                task.id
+              }}</router-link>
+            </th>
+            <td>
+              <router-link :to="'/tickets/' + task.id">
+                {{ task.name }}
+              </router-link>
+            </td>
+            <td>{{ task.status != "" ? task.status : "Open" }}</td>
+            <td>{{ task.dueDate != "" ? task.dueDate : "-" }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
